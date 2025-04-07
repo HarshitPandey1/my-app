cb8005cab9b53b60e91165927b2fcee3e327cfbd
Harshit <pharshit2003@gmail.com>
2025-04-07 10:23:16 +0530

    little improvements on searching

--- src/app/components/from-to-block/from-to-block.component.ts
+++ src/app/components/from-to-block/from-to-block.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
@@ -7,16 +8,47 @@ import { Router, RouterLink } from '@angular/router';
   templateUrl: './from-to-block.component.html',
   styleUrl: './from-to-block.component.css'
 })
-export class FromToBlockComponent {
-  constructor(private router: Router) {}
+export class FromToBlockComponent implements OnInit {
+  allFromOptions: string[] = [];
+  allToOptions: string[] = [];
+
+  filteredFromOptions: string[] = [];
+  filteredToOptions: string[] = [];
+
+  constructor(private router: Router, private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('http://localhost:3001/buses').subscribe(buses => {
+      this.allFromOptions = [...new Set(buses.map(bus => bus.from))];
+      this.allToOptions = [...new Set(buses.map(bus => bus.to))];
+    });
+  }
+
+  filterFrom(value: string): void {
+    this.filteredFromOptions = this.allFromOptions.filter(opt =>
+      opt.toLowerCase().includes(value.toLowerCase())
+    );
+  }
+
+  filterTo(value: string): void {
+    this.filteredToOptions = this.allToOptions.filter(opt =>
+      opt.toLowerCase().includes(value.toLowerCase())
+    );
+  }
+
+  selectFrom(option: string, input: HTMLInputElement) {
+    input.value = option;
+    this.filteredFromOptions = [];
+  }
+
+  selectTo(option: string, input: HTMLInputElement) {
+    input.value = option;
+    this.filteredToOptions = [];
+  }
 
   searchBuses(from: string, to: string, date: string) {
     this.router.navigate(['/available-bus'], {
-      queryParams: {
-        from,
-        to,
-        date
-      }
+      queryParams: { from, to, date }
     });
   }
 }
